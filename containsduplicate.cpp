bf1a9d82f5128cd67c2f33f88229c63191571094
Rajesh <malirajesh0705@gmail.com>
2023-09-27 21:56:46 +0530

    comp

--- containsduplicate.cpp
+++ containsduplicate.cpp
@@ -1,17 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int freq(vector<int> &nums, int element) {
-        int n = nums.size();
-        int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            if(element == nums[i]) cnt++;
-        }
-        return cnt;
-    }
+    // int freq(vector<int> &nums, int element) {
+    //     int n = nums.size();
+    //     int cnt = 0;
+    //     for(int i = 0; i < n; i++) {
+    //         if(element == nums[i]) cnt++;
+    //     }
+    //     return cnt;
+    // }
+    // bool containsDuplicate(vector<int>& nums) {
+    //     for(int i = 0; i < nums.size(); i++) {
+    //         if(freq(nums, nums[i]) == 2) return true;
+    //     }
+    //     return false;
+    // }
+    // bool containsDuplicate(vector<int>& nums) {
+    //     int n = nums.size();
+    //     sort(nums.begin(), nums.end());
+    //     for(int i = 0; i < n; i++) {
+    //         if(nums[i] == nums[i+1]) return true;
+    //     }
+    //     return false;
+    // }
     bool containsDuplicate(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++) {
-            if(freq(nums, nums[i]) == 2) return true;
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+
+        for(int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+        for(int i = 0; i < n; i++) {
+            if(mpp[nums[i]] == 2) return true;
         }
         return false;
     }
