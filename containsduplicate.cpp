aa0583aeec76691489cd68069487f050bc018d93
Rajesh <malirajesh0705@gmail.com>
2023-09-27 21:22:22 +0530

    completed

--- containsduplicate.cpp
+++ containsduplicate.cpp
@@ -1,7 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+    int freq(vector<int> &nums, int element) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(element == nums[i]) cnt++;
+        }
+        return cnt;
+    }
+    bool containsDuplicate(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(freq(nums, nums[i]) == 2) return true;
+        }
+        return false;
+    }
+
 int main() {
-    
+    int n, val; 
+    cin >> n;
+    vector<int> nums;
+    for(int i = 0; i < n; i++) {
+        cin >> val;
+        nums.push_back(val);
+    }
+    cout << containsDuplicate(nums);
     return 0;
 }
